58cd05ad967a4589428da5e2d839e7caaa9dfeda
Diana <dianap@outlook.cz>
2021-07-08 17:12:35 +0200

    added main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+print("This is your 2020 checklist! Let's start...")
+print("1. Did you think of sex at least once a day?")
+answer = input ("Say yes or no: ")
+if answer == "yes":
+    print(" Congratulations! You're a healthy individual!!! Please go on to next question. ")
+elif answer == "no":
+    print("God. You should do something about it. Call an expert, schedule a meeting, then go on to next question. ")
+print("2. Did you donate some money to a good cause?")
+answer = input ("Say yes or no: ")
+if answer == "yes":
+    print("Congratulations! You're a human being!!! Please go on to next question. ")
+elif answer == "no":
+    print("God. You should do something about it. Google an NGO that supports your favourite good cause, donate, then go on to next question. ")
+print("Did you experience unstoppable laughing for at least 12 times?")
+answer = input ("Say yes or no: ")
+if answer == "yes":
+    print("Great! All your wounds are healed. You can step into 2021!")
+elif answer == "no":
+    print("God. You should really do something about it, change your life. Make that a goal for 2021 before you enter it!")
